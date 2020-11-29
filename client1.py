ab83667dcfc8f89354f04001a2ed841f991b4f98
Vikram Kannan M <mail2me.mvk@gmail.com>
2020-11-29 02:44:12 -0500

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,29 +1,21 @@
-from rejson import Client, Path
-from timeloop import Timeloop
+
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson.json_util import dumps
 
-# start timeloop
-tl = Timeloop()
-
-# connect to queue
-rj = Client(host='redis', port=6379, decode_responses=True)
 
 #connect to database
-client = MongoClient('mongo', 27017)
+client = MongoClient('localhost', 27017)
 db = client.mydb
 
 app = Flask(__name__) # initialize the flask app
 
-rj.jsonset('tasks', Path.rootPath(), []) #initialize an empty array in the queue
 
 @app.route('/add_task',methods=['POST'])
-def send_to_queue():
-    print("Sending task to queue...")
+def add_task():
     data=request.get_json()
-    rj.jsonarrinsert('tasks', Path.rootPath(), 0,data)
-    return jsonify(data)
+    db['to-do'].insert_one(data)
+    return data
 
 @app.route('/get_tasks')
 def get_all_tasks():
