7af17c312254a0b982b697c6181f4b61f013b27b
Vikram Kannan M <mail2me.mvk@gmail.com>
2020-11-30 22:21:34 -0500

    Update client1.py

--- client1.py
+++ client1.py
@@ -2,8 +2,6 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson.json_util import dumps
-from flask_cors import cross_origin, CORS
-
 
 
 #connect to database
@@ -11,23 +9,20 @@ client = MongoClient('localhost', 27017)
 db = client.mydb
 
 app = Flask(__name__) # initialize the flask app
-CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 @app.route('/add_task',methods=['POST'])
-@cross_origin()
 def add_task():
     data=request.get_json()
     db['to-do'].insert_one(data)
     return dumps(data)
 
 @app.route('/get_tasks')
-@cross_origin()
 def get_all_tasks():
     data=list(db['to-do'].find())
     return dumps(data)
 
 @app.route('/update_task',methods=['POST'])
-@cross_origin()
 def update_task():
     task=request.get_json()['task']
     name=request.get_json()['name']
@@ -38,7 +33,6 @@ def update_task():
 
 
 @app.route('/delete_task',methods=['POST'])
-@cross_origin()
 def delete_task():
     task=request.get_json()['task']
     name=request.get_json()['name']
