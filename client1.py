e09c8642f828567ce206701c729a8c864d051356
Vikram Kannan M <mail2me.mvk@gmail.com>
2020-11-30 22:49:46 -0500

    Just added CORS code
    Only difference from what I submitted:
    
    @app.after_request
    def after_request(response):
        response.headers.add('Access-Control-Allow-Origin', '*')
        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
        response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
        return response

--- client1.py
+++ client1.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from pymongo import MongoClient
 from bson.json_util import dumps
 
@@ -10,6 +10,13 @@ db = client.mydb
 
 app = Flask(__name__) # initialize the flask app
 
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
 
 @app.route('/add_task',methods=['POST'])
 def add_task():
